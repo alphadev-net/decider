101665f7d87f2f97585d8b9d07af6c67245ac4de
Jan Seeger <jan@alphadev.net>
2011-06-30 16:02:43 +0200

    sdk version extracted strings to res

--- src/net/alphaDev/Decider/Decider.java
+++ src/net/alphaDev/Decider/Decider.java
@@ -4,6 +4,9 @@ import net.alphaDev.Decider.Actions.addAction;
 import net.alphaDev.Decider.Actions.decideAction;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -14,11 +17,14 @@ public class Decider extends Activity {
         super.onCreate(icicle);
         initComponents();
     }
-    
+
     private void initComponents() {
         setContentView(R.layout.main);
-        
-        adapter = new decideListAdapter(getApplicationContext());
+
+        if(adapter == null) {
+            adapter = new decideListAdapter(getApplicationContext());
+        }
+
         ((ListView) findViewById(R.id.list)).setAdapter(getListAdapter());
         ((Button) findViewById(R.id.addbtn)).setOnClickListener(new addAction(this));
         ((Button) findViewById(R.id.decidebtn)).setOnClickListener(new decideAction());
@@ -28,5 +34,29 @@ public class Decider extends Activity {
         return adapter;
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.decider_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about_btn:
+                showAbout();
+                return true;
+            case R.id.quit_btn:
+                System.exit(0);
+            default:
+                return super.onOptionsItemSelected(item);
+            }
+    }
+
     private ListAdapter adapter;
+
+    private void showAbout() {
+        
+    }
 }
